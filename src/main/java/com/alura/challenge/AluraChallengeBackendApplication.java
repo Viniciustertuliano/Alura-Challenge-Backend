e487cbf3125189653da38fe94eaead9486a04528
Vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2022-01-20 18:06:05 -0300

    Criado model de

---
+++ src/main/java/com/alura/challenge/AluraChallengeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.alura.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallengeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraChallengeBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/alura/challenge/AluraChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallengeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
